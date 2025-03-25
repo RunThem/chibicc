041ce0225b2f7cfa60ac80e15e848b99889b35b7
RunThem <iccy.fun@outlook.com>
2025-03-25 15:30:43 +0800

    [2] Add + and - operators

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+pub fn strtol(s: &str) -> (i32, &str) {
+  let (num, count) = lexical::parse_partial(s).unwrap();
+
+  (num, &s[count..])
+}
+
 fn main() {
   let args: Vec<_> = std::env::args().collect();
   if args.len() != 2 {
@@ -7,6 +13,27 @@ fn main() {
 
   println!("  .globl main");
   println!("main:");
-  println!("  mov ${}, %%rax", args[1].parse::<i32>().unwrap());
+
+  let (mut num, mut p) = strtol(args[1].as_str());
+  println!("  mov ${}, %%rax", num);
+
+  while !p.is_empty() {
+    match p.chars().nth(0) {
+      Some('+') => {
+        (num, p) = strtol(&p[1..]);
+        println!("  add ${}, %%rax", num);
+      }
+      Some('-') => {
+        (num, p) = strtol(&p[1..]);
+        println!("  sub ${}, %%rax", num);
+      }
+
+      ch => {
+        eprintln!("unexpected character: '{}'", ch.unwrap());
+        return;
+      }
+    }
+  }
+
   println!("  ret");
 }

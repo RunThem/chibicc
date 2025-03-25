1bb03955aeb4cc1c123619c872d0cca8cebaef7f
RunThem <iccy.fun@outlook.com>
2025-03-25 14:38:04 +0800

    [1] compile on integer to an exectuable that exits with the given number

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,12 @@
-fn main() {}
+fn main() {
+  let args: Vec<_> = std::env::args().collect();
+  if args.len() != 2 {
+    eprintln!("{}: invalid number of arguments", args[0]);
+    return;
+  }
+
+  println!("  .globl main");
+  println!("main:");
+  println!("  mov ${}, %%rax", args[1].parse::<i32>().unwrap());
+  println!("  ret");
+}
